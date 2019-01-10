fd8250d7396741fe41a952a6e5b70bfdff40b49f
ershing <ershing@foxmail.com>
2019-01-10 22:45:14 +0800

    add page

--- server/src/config/dict.js
+++ server/src/config/dict.js
@@ -1,20 +1,35 @@
 const earnDict = [
   { code: 1, value: '工资' },
   { code: 2, value: '奖金' },
-  { code: 3, value: '活期' },
-  { code: 4, value: '定期' },
-  { code: 5, value: '货币基金' },
-  { code: 6, value: '债券基金' },
-  { code: 7, value: '混合基金' },
-  { code: 8, value: '股票基金' },
-  { code: 9, value: '理财' },
-  { code: 10, value: '股票' },
-  { code: 11, value: '期货' },
-  { code: 12, value: '期权' },
-  { code: 13, value: '黄金' },
-  { code: 14, value: '石油' },
-  { code: 15, value: '外汇' }
+  { code: 3, value: '理财盈利' },
+  { code: 4, value: '红包' },
+  { code: 4, value: '收欠款' },
 ]
+
+const financingDict = [
+  { code: 1, value: '活期存储' },
+  { code: 2, value: '定期存储' },
+  { code: 3, value: '货币基金' },
+  { code: 4, value: '债券基金' },
+  { code: 5, value: '混合基金' },
+  { code: 6, value: '股票基金' },
+  { code: 7, value: '理财产品' },
+  { code: 8, value: '股票交易' },
+  { code: 9, value: '期货交易' },
+  { code: 10, value: '期权交易' },
+  { code: 11, value: '黄金交易' },
+  { code: 12, value: '石油交易' },
+  { code: 13, value: '外汇交易' },
+  { code: 14, value: '口袋现金' },
+  { code: 15, value: '电子零钱' }
+]
+
+const debtDict = [
+  { code: 2, value: '信用卡' },
+  { code: 3, value: '花呗' },
+  { code: 4, value: '京东白条' }
+]
+
 
 const periodDict = [
   { code: 1, value: '日' },
@@ -25,24 +40,44 @@ const periodDict = [
 ]
 
 const spendingDict = [
-  { code: 1, value: '' },
-  { code: 2, value: '' },
-  { code: 3, value: '' },
-  { code: 4, value: '' },
-  { code: 5, value: '' }
+  { code: 1, value: '衣服' },
+  { code: 2, value: '日常食品' },
+  { code: 2, value: '日常用品' },
+  { code: 3, value: '住宿' },
+  { code: 4, value: '交通' },
+  { code: 5, value: '油费' },
+  { code: 5, value: '路桥费' },
+  { code: 5, value: '汽车其他费用' },
+  { code: 5, value: '话费' },
+  { code: 5, value: '电影' },
+  { code: 5, value: '聚餐' },
+  { code: 5, value: '其他娱乐' },
+  { code: 5, value: '零食饮料' },
+  { code: 5, value: '电子设备' },
+  { code: 5, value: '教育' },
+  { code: 5, value: '医疗' },
+  { code: 5, value: '敬老' },
+  { code: 5, value: '礼品' },
+  { code: 5, value: '理财亏损' },
+  { code: 5, value: '借出' },
+  { code: 5, value: '还车贷' },
+  { code: 5, value: '还楼贷' },
+  { code: 5, value: '还装修贷' },
+  { code: 5, value: '还其他贷' },
 ]
 
 const assetDict = [
-  { code: 1, value: '' },
-  { code: 2, value: '' },
-  { code: 3, value: '' },
-  { code: 4, value: '' },
-  { code: 5, value: '' }
+  { code: 1, value: '微信' },
+  { code: 2, value: '支付宝' },
+  { code: 3, value: '银行卡' },
+  { code: 5, value: '股票账户' },
+  { code: 6, value: '外汇账户' },
+  { code: 7, value: '口袋现金' },
 ]
 
 const chargeDict = [
   { code: 1, value: '收入' },
   { code: 2, value: '支出' },
   { code: 3, value: '内部转账入' },
-  { code: 4, value: '内部转账出' }
+  { code: 4, value: '内部转账出' },
 ]
