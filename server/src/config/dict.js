d5c0cfc1e90e6a405f9dae82b78e0de6e23acc4c
ershing <ershing@foxmail.com>
2019-01-12 22:29:43 +0800

    change dict

--- server/src/config/dict.js
+++ server/src/config/dict.js
@@ -1,42 +1,43 @@
-const earnDict = [
-  { code: 1, value: '工资' },
-  { code: 2, value: '奖金' },
-  { code: 3, value: '理财盈利' },
-  { code: 4, value: '红包' },
-  { code: 4, value: '收欠款' },
-]
-
-const financingDict = [
-  { code: 1, value: '活期存储' },
-  { code: 2, value: '定期存储' },
-  { code: 3, value: '货币基金' },
-  { code: 4, value: '债券基金' },
-  { code: 5, value: '混合基金' },
-  { code: 6, value: '股票基金' },
-  { code: 7, value: '理财产品' },
-  { code: 8, value: '股票交易' },
-  { code: 9, value: '期货交易' },
-  { code: 10, value: '期权交易' },
-  { code: 11, value: '黄金交易' },
-  { code: 12, value: '石油交易' },
-  { code: 13, value: '外汇交易' },
-  { code: 14, value: '口袋现金' },
-  { code: 15, value: '电子零钱' }
+const assetDict = [
+  { code: 1, value: '现金' },  
+  { code: 2, value: '微信' },
+  { code: 3, value: '支付宝' },
+  { code: 4, value: '银行卡' },
+  { code: 5, value: '股票账户' },
+  { code: 6, value: '外汇账户' },
 ]
 
 const debtDict = [
-  { code: 2, value: '信用卡' },
-  { code: 3, value: '花呗' },
-  { code: 4, value: '京东白条' }
+  { code: 1, value: '信用卡' },
+  { code: 2, value: '花呗' },
+  { code: 3, value: '京东白条' }
 ]
 
+const financingDict = [
+  { code: 1, value: '口袋现金' },  
+  { code: 2, value: '活期存储' },
+  { code: 3, value: '定期存储' },
+  { code: 4, value: '电子零钱' },
+  { code: 5, value: '货币基金' },
+  { code: 6, value: '债券基金' },
+  { code: 7, value: '混合基金' },
+  { code: 8, value: '股票基金' },
+  { code: 9, value: '理财产品' },
+  { code: 10, value: '股票交易' },
+  { code: 11, value: '期货交易' },
+  { code: 12, value: '期权交易' },
+  { code: 13, value: '黄金交易' },
+  { code: 14, value: '石油交易' },
+  { code: 15, value: '外汇交易' },
+]
 
-const periodDict = [
-  { code: 1, value: '日' },
-  { code: 2, value: '周' },
-  { code: 3, value: '月' },
-  { code: 4, value: '季' },
-  { code: 5, value: '年' }
+const earnDict = [
+  { code: 1, value: '工资' },
+  { code: 2, value: '奖金' },
+  { code: 3, value: '盈利' },
+  { code: 4, value: '红包' },
+  { code: 4, value: '借入' },
+  { code: 4, value: '收欠款' },
 ]
 
 const spendingDict = [
@@ -54,30 +55,34 @@ const spendingDict = [
   { code: 5, value: '其他娱乐' },
   { code: 5, value: '零食饮料' },
   { code: 5, value: '电子设备' },
+  { code: 5, value: '旅游' },
   { code: 5, value: '教育' },
   { code: 5, value: '医疗' },
   { code: 5, value: '敬老' },
   { code: 5, value: '礼品' },
-  { code: 5, value: '理财亏损' },
+  { code: 5, value: '红包' },  
+  { code: 5, value: '亏损' },
   { code: 5, value: '借出' },
+  { code: 5, value: '还欠款' },
   { code: 5, value: '还车贷' },
   { code: 5, value: '还楼贷' },
   { code: 5, value: '还装修贷' },
-  { code: 5, value: '还其他贷' },
-]
-
-const assetDict = [
-  { code: 1, value: '微信' },
-  { code: 2, value: '支付宝' },
-  { code: 3, value: '银行卡' },
-  { code: 5, value: '股票账户' },
-  { code: 6, value: '外汇账户' },
-  { code: 7, value: '口袋现金' },
 ]
 
 const chargeDict = [
   { code: 1, value: '收入' },
   { code: 2, value: '支出' },
-  { code: 3, value: '内部转账入' },
-  { code: 4, value: '内部转账出' },
+  { code: 3, value: '内部转账出'}
+]
+
+const periodDict = [
+  { code: 1, value: '日' },
+  { code: 2, value: '周' },
+  { code: 3, value: '月' },
+  { code: 4, value: '季' },
+  { code: 5, value: '年' },
+  { code: 5, value: '周一到周五' },
+  { code: 5, value: '周六周日' },
+  { code: 5, value: '工作日' },
+  { code: 5, value: '节假日' },
 ]
